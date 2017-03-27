a2b44766bf7b0af2322aca9341821d23ccdf90c7
florestankorp <florestankorp@gmail.com>
2017-03-27 14:53:15 +0200

    Added Event model

---
+++ spec/factories/events.rb
@@ -0,0 +1,12 @@
+FactoryGirl.define do
+  factory :event do
+    title "MyString"
+    image "MyString"
+    location "MyString"
+    spaces "MyString"
+    available false
+    workshop "MyString"
+    sponsor "MyString"
+    public false
+  end
+end
