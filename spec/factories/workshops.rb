00fd2e0430ab167efa0f14ff8e81dd294f42e4a1
Dvessen83 <d.v.essen@gmail.com>
2017-03-27 14:41:04 +0200

    Added Workshop model

---
+++ spec/factories/workshops.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :workshop do
+    title "MyString"
+    description "MyText"
+    image "MyString"
+    link_live_demo "MyString"
+    link_github "MyString"
+  end
+end
