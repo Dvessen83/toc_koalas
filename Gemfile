662543926e2951bc8572610ac9a1e7c7027aaf3a
florestankorp <florestankorp@gmail.com>
2017-03-27 15:53:04 +0200

    Added validation tests

--- Gemfile
+++ Gemfile
@@ -41,6 +41,8 @@ group :development, :test do
   gem 'rspec-rails', '~> 3.5', '>= 3.5.2'
   gem 'factory_girl_rails', '4.7.0'
   gem 'faker', '1.6.6'
+  gem 'shoulda'
+  gem 'shoulda-matchers', '~> 3.1'
 end
 
 group :test do
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:location) }
+    it { should validate_presence_of(:spaces) }
+    it { should validate_presence_of(:workshop) }
+  end
+
+  describe "accepts input from factory" do
+    
+  end
+
+
 end
