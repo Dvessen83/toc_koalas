b5663c7699b091d075bdeea89548799120bbac6f
Xue1126 <tanxue1126@gmail.com>
2017-03-28 10:38:29 +0200

    test valid factory

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -3,14 +3,16 @@ require 'rails_helper'
 RSpec.describe Admin, type: :model do
 
   describe "validations" do
-      # before(:each) do
-      #   @admin = Factory.build(:admin)
-      # end
+    it "has a valid factory" do
+      expect(build(:admin).valid?).to eq(true)
+    end
+
     it "is invalid without a email address"  do
       admin = Admin.new(email: "")
       admin.valid?
       expect(admin.errors).to have_key(:email)
     end
+    
     it "is invalid without a password" do
       admin = Admin.new(password: "")
       admin.valid?
