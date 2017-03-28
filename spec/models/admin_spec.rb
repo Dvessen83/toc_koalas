818b3492d22fd7f687fe60c663ba2c9debb01e17
Xue1126 <tanxue1126@gmail.com>
2017-03-28 07:20:55 +0200

    test for email and password validation

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Admin, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "validations" do
+      # before(:each) do
+      #   @admin = Factory.build(:admin)
+      # end
+    it "is invalid without a email address"  do
+      admin = Admin.new(email: "")
+      admin.valid?
+      expect(admin.errors).to have_key(:email)
+    end
+    it "is invalid without a password" do
+      admin = Admin.new(password: "")
+      admin.valid?
+      expect(admin.errors).to have_key(:password)
+    end
+  end
 end
