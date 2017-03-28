5787ac8891226eab4663eba44c2715d621550448
SarahPAms <Sarah.L.Polan@gmail.com>
2017-03-28 10:19:49 +0200

    WIP tests need event model

--- spec/features/attendee_page_spec.rb
+++ spec/features/attendee_page_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
     describe "index action" do
     it "shows information about the attendees" do
       params = {"event_id"=>"1"}
+      visit event
       expect(page).to have_content "attendees"
     end
   end
