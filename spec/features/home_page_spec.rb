58f3c86c0e55ca794ef615a285f202f97384a068
Dvessen83 <d.v.essen@gmail.com>
2017-03-28 18:29:47 +0200

    Added home page test

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,9 +1,15 @@
 require 'rails_helper'
 
-describe "attendees Page" do
-  it "shows information about the organization" do
-    visit root_path
+describe 'Home Page' do
+    it 'renders the landing page of Taste of Code' do
+        visit root_path
 
-    expect(page).to have_content "Home"
+        expect(page).to have_content 'Taste of Code Beginner Coding Workshop'
     end
-  end
+
+    it 'should also render the buttons to go to different parts of the website' do
+        visit root_path
+
+        expect(page).to have_content'Join TCO InCompany Host TOC'
+    end
+end
