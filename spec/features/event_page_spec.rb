e0c1446dbe488ffcf225666ed41385a9a7fba85d
florestankorp <florestankorp@gmail.com>
2017-03-29 18:24:47 +0200

    Added what you will learn with tests

--- spec/features/event_page_spec.rb
+++ spec/features/event_page_spec.rb
@@ -7,3 +7,21 @@ describe "Attendee Page" do
     expect(page).to have_content "Events"
   end
 end
+
+describe "what you will learn section" do
+  it "shows HTML" do
+    visit root_path
+
+    expect(page).to have_content "HTML"
+  end
+  it "shows CSS" do
+    visit root_path
+
+    expect(page).to have_content "CSS"
+  end
+  it "shows JavaScript" do
+    visit root_path
+
+    expect(page).to have_content "JavaScript"
+  end
+end
