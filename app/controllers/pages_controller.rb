e9ba7e7372b3fd26226206391295dad284bca3a2
SarahPAms <Sarah.L.Polan@gmail.com>
2017-03-29 10:58:06 +0200

    updated routes for attend, incompany, and sponsor with rspec tests

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
-  def index
+  def home
+  end
+
+  def attend
+  end
+
+  def incompany
+  end
+
+  def sponsor
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 
 
   root to: "pages#home"
-
+  get "attend" => "pages#attend"
+  get "incompany" => "pages#incompany"
+  get "sponsor" => "pages#sponsor"
   devise_for :admins
 
   resources :pages
